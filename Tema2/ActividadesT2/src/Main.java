5cb8d88634e082d4d9d5f8e1bda8f8e127d42c6c
gmartin443 <guillermomartinchippirraz@gmail.com>
2025-10-09 19:57:54 +0200

    v2.0

---
+++ Tema2/ActividadesT2/src/Main.java
@@ -0,0 +1,45 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        Actividad1 hilo1 = new Actividad1("Primer hilo");
+//        Actividad1 hilo2 = new Actividad1("Segundo hilo");
+//        Actividad1 hilo3 = new Actividad1("Tercer hilo");
+//
+//        hilo1.start();
+//        hilo2.start();
+//        hilo3.start();
+
+//        Thread hilo1 = new Thread(new Actividad2("Primer hilo"));
+//        Thread hilo2 = new Thread(new Actividad2("Segundo hilo"));
+//        Thread hilo3 = new Thread(new Actividad2("Tercer hilo"));
+//
+//        hilo1.start();
+//        hilo2.start();
+//        hilo3.start();
+
+        Actividad3 dado1 = new Actividad3();
+        Actividad3 dado2 = new Actividad3();
+        Actividad3 dado3 = new Actividad3();
+
+        dado1.setName("Hilo 1");
+        dado2.setName("Hilo 2");
+        dado3.setName("Hilo 3");
+
+        dado1.start();
+        dado2.start();
+        dado3.start();
+
+        try{
+            dado1.join();
+            dado2.join();
+            dado3.join();
+        }catch (InterruptedException e){
+            System.out.println("Error al esperar los hilos.");
+        }
+
+        int suma = dado1.getResultado() + dado2.getResultado() + dado3.getResultado();
+        System.out.println("Suma total de las tiradas: " + suma);
+    }
+}
---
+++ Tema2/PracticaT2/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class Main {
+    static final int DIAS = 18250;
+    static final int MIN_TEMP = -20;
+    static final int MAX_TEMP = 50;
+    public static void main(String[] args) throws InterruptedException{
+        int[] temp = new int[DIAS];
+        int numHilos = Integer.parseInt(args[0]);
+        Random rand = new Random();
+
+        for (int i = 0; i < DIAS; i++){
+            temp[i] = rand.nextInt(MAX_TEMP - MIN_TEMP + 1) + MIN_TEMP;
+        }
+
+        Actividad1[] hilos = new Actividad1[numHilos];
+        int segSize = DIAS/numHilos;
+
+        for (int i = 0; i < numHilos; i++){
+            int inicio = i * segSize;
+            int fin = (i == numHilos - 1) ? DIAS : inicio + segSize;
+            hilos[i] = new Actividad1(temp, inicio, fin);
+            hilos[i].start();
+        }
+
+        double sumaPromedios = 0;
+        for (Actividad1 hilo : hilos){
+            hilo.join();
+            sumaPromedios += hilo.getPromedio();
+        }
+
+        double promedioFinal = sumaPromedios / numHilos;
+
+        System.out.println("Temperatura promedio de los últimos 50 años: " + promedioFinal + " ºC.");
+    }
+}
